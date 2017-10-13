7e1b9f7a66203e3ca4e3ccd3d2c55af06966270f
2Deo <ziemowit.holda@gmail.com>
2017-10-13 11:46:42 +0200

    Through Association: Controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :events
+
+  resources :events do
+    resources :registrations, only: [:create]
+  end
+
   resources :profiles, only: [:new, :edit, :create, :update]
 
   get "about" => "pages#about"
   get "register_for_event" => "pages#register"
-  
+
 
   resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
